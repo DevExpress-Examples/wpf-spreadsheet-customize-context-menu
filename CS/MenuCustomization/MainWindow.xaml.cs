a809b600615c4ce4294baa35a724cc02de08b206
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/MenuCustomization/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using DevExpress.Xpf.Bars;
+using DevExpress.Xpf.Spreadsheet;
+using DevExpress.Xpf.Spreadsheet.Menu;
+using DevExpress.XtraSpreadsheet.Commands;
+
+namespace MenuCustomization
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : DevExpress.Xpf.Core.DXWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            spreadsheetControl1.Document.Worksheets[0].Cells["B2"].Value = "Right-click any cell or column header to display the custom context menu";
+        }
+
+        #region #PopupMenuShowing
+        private void SpreadsheetControlPopupMenuShowing(object sender, PopupMenuShowingEventArgs e)
+        {
+            // Check whether this event was raised for a cell popup menu of the Spreadsheet control.
+            if (e.MenuType == SpreadsheetMenuType.Cell)
+            {
+                // Remove the "Clear Contents" menu item.
+                e.Customizations.Add(new RemoveSpreadsheetCommandAction() { Id = SpreadsheetCommandId.FormatClearContentsContextMenuItem });
+
+                // Create a menu item for the Spreadsheet command, which inserts a picture into a worksheet.
+                e.Customizations.Add(new BarButtonItem() { Command = SpreadsheetUICommand.InsertPicture,
+                    Content = "Insert Picture", CommandParameter = spreadsheetControl1 });
+
+                // Create a custom menu item and handle its click event.
+                BarButtonItem menuItem = new BarButtonItem();
+                menuItem.Name = "customHighlightItem";
+                menuItem.Content = "Highlight Selection";
+                menuItem.ItemClick += new ItemClickEventHandler(customHighlightItem_ItemClick);
+                e.Customizations.Add(menuItem);
+            }
+        }
+
+            private void customHighlightItem_ItemClick(object sender, ItemClickEventArgs e)
+        {
+            // Fill the selection with the yellow color. 
+            spreadsheetControl1.Selection.FillColor = System.Drawing.Color.Yellow;
+        }
+        #endregion #PopupMenuShowing
+    }
+}
---
+++ VB/MenuCustomization/MainWindow.xaml.vb
@@ -0,0 +1,47 @@
+﻿Imports DevExpress.Xpf.Bars
+Imports DevExpress.Xpf.Spreadsheet
+Imports DevExpress.Xpf.Spreadsheet.Menu
+Imports DevExpress.XtraSpreadsheet.Commands
+
+Namespace MenuCustomization
+    ''' <summary>
+    ''' Interaction logic for MainWindow.xaml
+    ''' </summary>
+    Partial Public Class MainWindow
+        Inherits DevExpress.Xpf.Core.DXWindow
+
+        Public Sub New()
+            InitializeComponent()
+            spreadsheetControl1.Document.Worksheets(0).Cells("B2").Value = "Right-click any cell or column header to display the custom context menu"
+        End Sub
+
+        #Region "#PopupMenuShowing"
+        Private Sub SpreadsheetControlPopupMenuShowing(ByVal sender As Object, ByVal e As PopupMenuShowingEventArgs)
+            ' Check whether this event was raised for a cell popup menu of the Spreadsheet control.
+            If e.MenuType = SpreadsheetMenuType.Cell Then
+                ' Remove the "Clear Contents" menu item.
+                e.Customizations.Add(New RemoveSpreadsheetCommandAction() With {.Id = SpreadsheetCommandId.FormatClearContentsContextMenuItem})
+
+                ' Create a menu item for the Spreadsheet command, which inserts a picture into a worksheet.
+                e.Customizations.Add(New BarButtonItem() With { _
+                    .Command = SpreadsheetUICommand.InsertPicture, _
+                    .Content = "Insert Picture", _
+                    .CommandParameter = spreadsheetControl1 _
+                })
+
+                ' Create a custom menu item and handle its click event.
+                Dim menuItem As New BarButtonItem()
+                menuItem.Name = "customHighlightItem"
+                menuItem.Content = "Highlight Selection"
+                AddHandler menuItem.ItemClick, AddressOf customHighlightItem_ItemClick
+                e.Customizations.Add(menuItem)
+            End If
+        End Sub
+
+            Private Sub customHighlightItem_ItemClick(ByVal sender As Object, ByVal e As ItemClickEventArgs)
+            ' Fill the selection with the yellow color. 
+            spreadsheetControl1.Selection.FillColor = System.Drawing.Color.Yellow
+            End Sub
+        #End Region ' #PopupMenuShowing
+    End Class
+End Namespace
