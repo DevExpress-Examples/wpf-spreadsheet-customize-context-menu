7eb71dc55c3d82adedc0e2aa4668ba0418f92ab9
DevExpressExampleBot <fakeEmail>
2022-04-20 13:57:56 +0400

    Source auto update [skip ci]

---
+++ VB/WpfSpreadsheetMenuCustomization/MainWindow.xaml.vb
@@ -0,0 +1,21 @@
+Namespace WpfSpreadsheetMenuCustomization
+
+    ''' <summary>
+    ''' Interaction logic for MainWindow.xaml
+    ''' </summary>
+    Public Partial Class MainWindow
+        Inherits DevExpress.Xpf.Core.ThemedWindow
+
+        Public Sub New()
+            Me.InitializeComponent()
+            Me.spreadsheetControl.ActiveWorksheet.Cells("B2").Value = "Right-click a cell to display the custom context menu"
+        End Sub
+
+#Region "HighlightCells"
+        Private Sub HighlightCells_ItemClick(ByVal sender As Object, ByVal e As DevExpress.Xpf.Bars.ItemClickEventArgs)
+            ' Fill selected cells with the yellow color. 
+            Me.spreadsheetControl.Selection.FillColor = System.Drawing.Color.Yellow
+        End Sub
+#End Region
+    End Class
+End Namespace
